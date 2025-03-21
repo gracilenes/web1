3e4870355adb5b8877cd76908c3ef641efd9c025
gracilenes <gracilenes876@gmail.com>
2025-03-21 18:48:21 +0000

    atv

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
     
-    const username= document.getElementById("username"). value;
-    const password= document.getElementById("password"). value;
+    const username= document.getElementById("username").value;
+    const password= document.getElementById("password").value;
     const message= document.getElementById("message");
 
     const userCorrect = "admin";
@@ -13,7 +13,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
         message.textContent = "Login bem-sucedido!";
         setTimeout(() => {
             window.location.href = "dashboard.html"
-        } , 1500);
+        });
     } else {
         message.style.color = "red";
         message.textContent = "Usuário ou Senha Incorretos!"
